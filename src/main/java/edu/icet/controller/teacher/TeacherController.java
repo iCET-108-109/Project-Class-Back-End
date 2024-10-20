a1e6345fedd461bddd8fff17fc63f2b7f471ad6a
NesanduA <nesanduanusara73@gmail.com>
2024-10-20 17:48:23 +0530

    Search by teacher id done

--- src/main/java/edu/icet/controller/teacher/TeacherController.java
+++ src/main/java/edu/icet/controller/teacher/TeacherController.java
@@ -1,7 +1,10 @@
 package edu.icet.controller.teacher;
 
+import edu.icet.dto.Teacher;
 import edu.icet.service.teacher.TeacherService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TeacherController {
     final TeacherService teacherService;
+
+    @GetMapping("/Search-By-Id/{id}")
+    public Teacher searchById(@PathVariable Integer id){
+        return teacherService.searchById(id);
+    }
 }
