9724ab3bc05d3d91f821d3f388e095e035b4c12f
icet-sharadha <icet.sharadha@gmail.com>
2024-10-20 14:52:43 +0530

    commit

---
+++ src/main/java/edu/icet/icet_student_management_system/Main.java
@@ -0,0 +1,13 @@
+package edu.icet.icet_student_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
---
+++ src/test/java/edu/icet/icet_student_management_system/IcetStudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.icet.icet_student_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcetStudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
