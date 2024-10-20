09070603f424168e441c819c4a7ae20efb1c5bfd
Tharush2002 <tharush1205@gmail.com>
2024-10-20 16:58:37 +0530

    completed dto and entities

--- src/main/java/edu/icet/Main.java
+++ src/main/java/edu/icet/Main.java
@@ -1,4 +1,4 @@
-package edu.icet.icet_student_management_system;
+package edu.icet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
